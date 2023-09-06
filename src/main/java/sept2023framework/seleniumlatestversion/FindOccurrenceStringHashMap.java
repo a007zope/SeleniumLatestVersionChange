706349b9b17c81121f12d42d978bf0cdc277b899
a007zope <a007zope@gmail.com>
2023-09-06 12:53:37 +0530

    latestCodeChangesrelatedtooccurrrenceinastring

--- src/main/java/sept2023framework/seleniumlatestversion/FindOccurrenceStringHashMap.java
+++ src/main/java/sept2023framework/seleniumlatestversion/FindOccurrenceStringHashMap.java
@@ -8,14 +8,17 @@ public class FindOccurrenceStringHashMap {
 	
 	public static void main(String[] args) {
 		
-		String name = "Alice is wondeful and Aditya is handsome but Aditya rocks and Alice flops";
+		String name = "Alice is wondeful and Aditya is handsome but Aditya rocks and Alice flops Also Aditya is Aditya Zope Aditya Alice Aditya Aditya Aditya ";
 		
 		Map<String, Integer> nameMap = new HashMap<>();
 		
 		        String [] nameArray = name.split(" ");
 		        
+		      
+		        
 		        for(String nameList : nameArray)
 		        {
+		        	  System.out.println(nameMap.get(nameList));
 		        	if(nameMap.containsKey(nameList))
 		        	{
 		        		nameMap.put(nameList,nameMap.get(nameList)+1);
